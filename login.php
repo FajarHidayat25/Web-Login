e1805cbacdd5c1f37698b8fef1e4312b49797be2
FajarHidayat25 <129923915+FajarHidayat25@users.noreply.github.com>
2023-06-29 20:31:29 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  // Check if the username and password are correct
+  // If correct, redirect to the home page
+  // If incorrect, display an error message
+?>
