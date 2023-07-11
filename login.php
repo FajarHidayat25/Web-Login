3229d370762181309c6769da73c0cc12439c2b5e
FajarHidayat25 <129923915+FajarHidayat25@users.noreply.github.com>
2023-07-11 21:24:32 +0700

    Delete login.php

--- login.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-
-  // Check if the username and password are correct
-  // If correct, redirect to the home page
-  // If incorrect, display an error message
-?>
